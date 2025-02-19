245686f8f0c3097c1d73031e531178628af7da32
j-ansh <anshjamulla07@gmail.com>
2025-02-20 00:47:20 +0530

    Backend server added

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "https://todo-app-fullstack-jtp2.onrender.com/api";
 const api = axios.create({
     baseURL: BASE_URL,
     headers:{
