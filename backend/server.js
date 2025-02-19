3efee74d385b2f05b9727e572b652fbe2de9f247
j-ansh <anshjamulla07@gmail.com>
2025-02-19 23:40:38 +0530

    server update

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,11 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", todoRoutes);
 
-mongoose.connect(process.env.MONGO_URI).then(()=>console.log("MongoDB connected"))
-.catch(err => console.log(err)) 
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB connected"))
+.catch((err) => {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1)}); 
 
 app.listen(PORT, ()=>{
     console.log(`app running on ${PORT}`)
